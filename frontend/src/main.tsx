2ca68115351be7db889eb825ec487250f3e49a31
veryval <veryval@mail.ru>
2025-10-11 20:42:55 +0300

    fix back healthcheck

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,7 @@ import App from './App.tsx';
 import PublicExerciseView from './components/PublicExerciseView.tsx'; 
 import './index.css';
 
-// Способ 1: Используем абсолютный путь для public файлов
 const backgroundUrl = '/background.png';
-
-// Способ 2: Или импортируем через new URL (более надежно)
-// const backgroundUrl = new URL('../public/background.png', import.meta.url).href;
 
 document.body.style.backgroundImage = `url(${backgroundUrl})`;
 document.body.style.backgroundSize = 'cover';
